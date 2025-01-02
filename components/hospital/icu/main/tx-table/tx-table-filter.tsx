0ffb585acd61173f727c5c20611eac274f4cee8c
Kled <1017yu@daum.net>
2025-01-02 16:13:53 +0900

    feat: tx-table 처치표 내 카테고리 다중 선택

--- components/hospital/icu/main/tx-table/tx-table-filter.tsx
+++ components/hospital/icu/main/tx-table/tx-table-filter.tsx
@@ -1,10 +1,12 @@
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
+  MultiSelector,
+  MultiSelectorContent,
+  MultiSelectorInput,
+  MultiSelectorItem,
+  MultiSelectorList,
+  MultiSelectorReset,
+  MultiSelectorTrigger,
+} from '@/components/ui/multi-select'
 import { DEFAULT_ICU_ORDER_TYPE } from '@/constants/hospital/icu/chart/order'
 import { cn } from '@/lib/utils/utils'
 import { useBasicHosDataContext } from '@/providers/basic-hos-data-context-provider'
@@ -14,46 +16,54 @@ export default function TxTableFilter({
   localFilterState,
   setLocalFilterState,
 }: {
-  localFilterState: string
-  setLocalFilterState: Dispatch<SetStateAction<string>>
+  localFilterState: string[]
+  setLocalFilterState: Dispatch<SetStateAction<string[]>>
 }) {
   const {
     basicHosData: { orderColorsData },
   } = useBasicHosDataContext()
 
   return (
-    <Select value={localFilterState} onValueChange={setLocalFilterState}>
-      <SelectTrigger
+    <MultiSelector
+      values={localFilterState}
+      options={DEFAULT_ICU_ORDER_TYPE.map((type) => ({
+        value: type.value,
+        label: type.label,
+      }))}
+      onValuesChange={setLocalFilterState}
+      className="w-full"
+    >
+      <MultiSelectorTrigger
         className={cn(
-          'fixed left-2 top-[54px] z-30 w-[calc(100vw-16px)] 2xl:left-auto 2xl:right-2 2xl:top-1.5 2xl:w-[240px]',
+          'fixed left-2 top-[48px] z-40 w-[calc(100vw-16px)] 2xl:left-auto 2xl:right-2 2xl:top-[-4px] 2xl:w-[640px]',
         )}
       >
-        <SelectValue />
-      </SelectTrigger>
+        <MultiSelectorInput placeholder="오더 타입 선택" />
+      </MultiSelectorTrigger>
 
-      <SelectContent>
-        <SelectItem key="all" value="all">
-          전체
-        </SelectItem>
-
-        {DEFAULT_ICU_ORDER_TYPE.map((type) => (
-          <SelectItem
-            key={type.value}
-            value={type.value}
-            className="rounded-none transition hover:opacity-70"
-          >
-            <div className="flex items-center gap-2">
-              <div
-                className="h-4 w-4 rounded-full border"
-                style={{
-                  backgroundColor: orderColorsData[type.value],
-                }}
-              />
-              {type.label}
-            </div>
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
+      <MultiSelectorContent>
+        <MultiSelectorList className="fixed left-2 top-[104px] z-40 w-[calc(100vw-16px)] 2xl:left-auto 2xl:right-2 2xl:top-12 2xl:w-[640px]">
+          <MultiSelectorReset />
+          {DEFAULT_ICU_ORDER_TYPE.map((type) => (
+            <MultiSelectorItem
+              key={type.value}
+              value={type.value}
+              label={type.label}
+              className="rounded-none transition hover:opacity-70"
+            >
+              <div className="flex items-center gap-2">
+                <div
+                  className="h-4 w-4 rounded-full border"
+                  style={{
+                    backgroundColor: orderColorsData[type.value],
+                  }}
+                />
+                {type.label}
+              </div>
+            </MultiSelectorItem>
+          ))}
+        </MultiSelectorList>
+      </MultiSelectorContent>
+    </MultiSelector>
   )
 }
