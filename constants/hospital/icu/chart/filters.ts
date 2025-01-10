66b1e7cf147cce4a5898223bb26a9d2c04d9680a
Kled <1017yu@daum.net>
2025-01-10 13:55:34 +0900

    refactor: 환자 정렬 필터 `LocalStorage`로 리팩토링

--- constants/hospital/icu/chart/filters.ts
+++ constants/hospital/icu/chart/filters.ts
@@ -1,6 +1,6 @@
-import type { FilterState } from '@/types/icu/chart'
+import type { Filter } from '@/types/icu/chart'
 
-export const DEFAULT_FILTER_STATE: FilterState = {
+export const DEFAULT_FILTER_STATE: Filter = {
   selectedGroup: [],
   selectedVet: '',
   selectedSort: 'date',
