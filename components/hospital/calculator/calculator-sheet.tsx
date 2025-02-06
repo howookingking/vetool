9edf0b1d23d4077a44497552492b1d90d8da493b
Kled <1017yu@daum.net>
2025-02-06 14:11:21 +0900

    design: calculator 반응형 작업

--- components/hospital/calculator/calculator-sheet.tsx
+++ components/hospital/calculator/calculator-sheet.tsx
@@ -3,21 +3,25 @@
 import CalculatorSheetContent from '@/components/hospital/calculator/calculator-sheet-content'
 import { Button } from '@/components/ui/button'
 import { Sheet, SheetTrigger } from '@/components/ui/sheet'
+import useIsMobile from '@/hooks/use-is-mobile'
 import { Calculator } from 'lucide-react'
 import { useState } from 'react'
+import { cn } from '@/lib/utils/utils'
 
 export default function CalculatorSheet() {
+  const isMobile = useIsMobile()
+
   const [isSheetOpen, setIsSheetOpen] = useState(false)
 
   return (
     <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
-      <SheetTrigger asChild>
+      <SheetTrigger className={cn(isMobile && 'm-1')} asChild>
         <Button size="icon" className="h-10 w-10 rounded-full">
           <Calculator />
         </Button>
       </SheetTrigger>
 
-      <CalculatorSheetContent isSheetOpen={isSheetOpen} />
+      <CalculatorSheetContent isSheetOpen={isSheetOpen} isMobile={isMobile} />
     </Sheet>
   )
 }
--- components/hospital/calculator/calculator-sidebar.tsx
+++ components/hospital/calculator/calculator-sidebar.tsx
@@ -3,6 +3,7 @@ import { CALCULATORS } from '@/constants/hospital/icu/calculator/calcularor'
 import { cn } from '@/lib/utils/utils'
 import { type SelectedCalculator } from '@/types/hospital/calculator'
 import { Dispatch, SetStateAction } from 'react'
+
 type CalculatorSidebarProps = {
   selectedCalculator: SelectedCalculator
   setSelectedCalculator: Dispatch<SetStateAction<SelectedCalculator>>
@@ -13,13 +14,13 @@ export default function CalculatorSidebar({
   setSelectedCalculator,
 }: CalculatorSidebarProps) {
   return (
-    <ul className="flex h-screen w-[200px] flex-col border-r">
+    <ul className="mx-2 mb-2 mt-12 flex flex-row justify-between rounded-lg border sm:m-0 sm:w-[240px] sm:flex-col sm:justify-start">
       {CALCULATORS.map((calculator) => (
         <li key={calculator.value}>
           <Button
             className={cn(
               selectedCalculator === calculator.value && 'bg-muted',
-              'w-full rounded-none',
+              'text-md w-full rounded-none py-6',
             )}
             variant="ghost"
             onClick={() => setSelectedCalculator(calculator.value)}
