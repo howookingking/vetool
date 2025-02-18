2a85e0f1d7b859aada5fa8682c51f7a473024718
Jungwoo Lee <junsgk@naver.com>
2025-02-18 16:25:40 +0900

    refactor: BSA

--- components/hospital/calculator/bsa/bsa.tsx
+++ components/hospital/calculator/bsa/bsa.tsx
@@ -6,33 +6,16 @@ import {
   SheetHeader,
   SheetTitle,
 } from '@/components/ui/sheet'
-import { type PatientFormData } from '@/types/hospital/calculator'
 import { VisuallyHidden } from '@radix-ui/react-visually-hidden'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import CalculatorResult from '../result/calculator-result'
 
 export default function Bsa({ weight }: { weight: string }) {
-  const [result, setResult] = useState<number | null>(null)
-  const [formData, setFormData] = useState<PatientFormData>({
-    weight: weight,
-  })
+  const [localWeight, setLocalWeight] = useState(weight)
 
-  const calculateBsa = (weight: number): number =>
-    Number((0.1 * Math.pow(weight, 2 / 3)).toFixed(2))
-
-  useEffect(() => {
-    if (weight) {
-      setFormData({
-        weight: weight ?? '0',
-      })
-    }
-  }, [weight])
-
-  useEffect(() => {
-    if (formData.weight) {
-      setResult(calculateBsa(Number(formData.weight)))
-    }
-  }, [formData.weight, formData.species])
+  const result =
+    localWeight !== '' &&
+    Number((0.1 * Math.pow(Number(localWeight), 2 / 3)).toFixed(2))
 
   return (
     <div className="flex flex-col gap-4">
@@ -46,18 +29,22 @@ export default function Bsa({ weight }: { weight: string }) {
         </VisuallyHidden>
       </SheetHeader>
 
-      <div className="space-y-2">
-        <Label>체중 (kg)</Label>
+      <div className="relative w-1/2">
+        <Label htmlFor="weight">체중</Label>
         <Input
           type="number"
-          placeholder="체중을 입력하세요"
-          value={formData.weight}
-          className="w-1/2"
-          onChange={(e) => setFormData({ ...formData, weight: e.target.value })}
+          id="weight"
+          className="mt-1"
+          value={localWeight}
+          onChange={(e) => setLocalWeight(e.target.value)}
+          placeholder="체중"
         />
+        <span className="absolute bottom-2 right-2 text-sm text-muted-foreground">
+          kg
+        </span>
       </div>
 
-      {result !== null && result >= 0 && (
+      {result && (
         <CalculatorResult
           displayResult={
             <span className="font-bold text-primary">
