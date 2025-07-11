a8848d85e566ddaf2c0f875cc5a3a9fe29ef3ec0
Jungwoo Lee <junsgk@naver.com>
2025-07-11 16:24:24 +0900

    Update user-key-guide-message.tsx

--- components/hospital/icu/main/chart/selected-chart/chart-body/table/chart-table-body/order-creator/user-key-guide-message.tsx
+++ components/hospital/icu/main/chart/selected-chart/chart-body/table/chart-table-body/order-creator/user-key-guide-message.tsx
@@ -7,13 +7,14 @@ export default function UserKeyGuideMessage({ isDT }: { isDT?: boolean }) {
       <div className="absolute bottom-3 left-2 hidden items-center gap-2 whitespace-nowrap text-muted-foreground md:flex">
         <div>
           <span className="mx-1 bg-rose-400/10 p-1">형광팬</span> : CTRL +
-          우클릭
+          처치칸 우클릭
         </div>
 
         {!isDT && (
           <>
             <Separator orientation="vertical" className="h-4" />
-            다중선택 : CTRL + 오더 또는 처치칸 좌클릭
+            <div>다중선택 : CTRL + 오더 또는 처치칸 좌클릭</div>
+
             <Separator orientation="vertical" className="h-4" />
             <div>처치칸에 &quot;처치결과$코멘트&quot; 입력가능</div>
           </>
