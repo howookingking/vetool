e82c92dd101aa7d9f924eed4de92abcbd597c92d
Jungwoo Lee <junsgk@naver.com>
2025-07-21 11:07:59 +0900

    메세지 draggable 적용

--- components/common/new-feature.tsx
+++ components/common/new-feature.tsx
@@ -3,25 +3,28 @@ import { cn } from '@/lib/utils/utils'
 import React from 'react'
 
 export default function NewFeature({
-  LocalStoragekey,
+  // LocalStoragekey,
   children,
   className,
 }: {
-  LocalStoragekey: string
+  // LocalStoragekey: string
   children: React.ReactNode
   className?: string
 }) {
-  const [hasSeen, setHasSeen] = useLocalStorage(LocalStoragekey, false)
+  // const [hasSeen, setHasSeen] = useLocalStorage(LocalStoragekey, false)
   return (
-    <div className="relative" onClick={() => setHasSeen(true)}>
-      {!hasSeen && (
-        <div
-          className={cn(
-            'absolute right-1.5 top-1.5 h-1.5 w-1.5 rounded-full bg-destructive',
-            className,
-          )}
-        />
-      )}
+    <div
+      className="relative"
+      // onClick={() => setHasSeen(true)}
+    >
+      {/* {!hasSeen && ( */}
+      <div
+        className={cn(
+          'absolute right-1.5 top-1.5 h-1.5 w-1.5 rounded-full bg-destructive',
+          className,
+        )}
+      />
+      {/* )} */}
 
       <div>{children}</div>
     </div>
--- components/hospital/message/message.tsx
+++ components/hospital/message/message.tsx
@@ -4,8 +4,10 @@ import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import type { VetoolUser } from '@/lib/services/auth/authorization'
 import { MessageCircle, X } from 'lucide-react'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
+import Draggable from 'react-draggable'
 import MessageWindow from './message-window'
+import NewFeature from '@/components/common/new-feature'
 
 type Props = {
   loggdedInUser: VetoolUser
@@ -14,6 +16,7 @@ type Props = {
 
 export default function Message({ loggdedInUser, hosId }: Props) {
   const [isOpen, setIsOpen] = useState(false)
+  const nodeRef = useRef(null)
 
   return (
     <>
@@ -24,20 +27,29 @@ export default function Message({ loggdedInUser, hosId }: Props) {
           className="rounded-full"
           aria-label={isOpen ? '채팅 닫기' : '채팅 열기'}
         >
-          <MessageCircle />
+          <NewFeature className="-right-1 -top-1">
+            <MessageCircle />
+          </NewFeature>
         </Button>
       </div>
 
       {isOpen && (
-        <Card className="fixed bottom-2 right-2 z-50 ml-2 min-w-[300px] overflow-hidden md:min-w-[400px]">
-          <X
-            className="absolute right-1 top-1 z-50 h-4 w-4 cursor-pointer"
-            onClick={() => setIsOpen(false)}
-            aria-label="채팅 닫기"
-          />
+        <Draggable handle=".handle" nodeRef={nodeRef}>
+          <Card
+            ref={nodeRef}
+            className="fixed bottom-2 right-2 z-50 ml-2 min-w-[300px] overflow-hidden md:min-w-[400px]"
+          >
+            <div className="handle absolute left-1/2 top-1 z-50 h-1.5 w-12 -translate-x-1/2 cursor-move rounded-md bg-gray-50" />
 
-          <MessageWindow loggdedInUser={loggdedInUser} hosId={hosId} />
-        </Card>
+            <X
+              className="absolute right-1 top-1 z-50 h-4 w-4 cursor-pointer"
+              onClick={() => setIsOpen(false)}
+              aria-label="채팅 닫기"
+            />
+
+            <MessageWindow loggdedInUser={loggdedInUser} hosId={hosId} />
+          </Card>
+        </Draggable>
       )}
     </>
   )
