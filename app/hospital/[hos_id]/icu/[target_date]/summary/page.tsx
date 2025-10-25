a21000e956dcffe7306b585b0583b5fe718c1817
Jungwoo Lee <junsgk@naver.com>
2025-10-25 20:54:18 +0900

    summary

--- app/hospital/[hos_id]/icu/[target_date]/summary/page.tsx
+++ app/hospital/[hos_id]/icu/[target_date]/summary/page.tsx
@@ -2,8 +2,8 @@ import NoResultSquirrel from '@/components/common/no-result-squirrel'
 import SummaryInfos from '@/components/hospital/icu/main/summary/summary-infos/summary-infos'
 import SummaryTable from '@/components/hospital/icu/main/summary/table/summary-table'
 import { fetchSummaryData } from '@/lib/services/icu/summary/fetch-summary-data'
-import MobileTitle from '../../../../../../components/common/mobile-title'
 import { DashboardIcon } from '@radix-ui/react-icons'
+import MobileTitle from '../../../../../../components/common/mobile-title'
 
 export default async function SummaryPage(props: {
   params: Promise<{
@@ -15,37 +15,31 @@ export default async function SummaryPage(props: {
   const { prev_summary_count, target_date_summary_data } =
     await fetchSummaryData(hos_id, target_date)
 
-  if (target_date_summary_data.length === 0) {
-    return (
-      <>
-        <MobileTitle title="종합현황" icon={DashboardIcon} />
+  return (
+    <>
+      <MobileTitle title="종합현황" icon={DashboardIcon} />
 
+      {target_date_summary_data.length === 0 ? (
         <NoResultSquirrel
           text="입원환자 없음"
           className="h-mobile flex-col 2xl:h-desktop"
           size="lg"
         />
-      </>
-    )
-  }
+      ) : (
+        <div className="space-y-2 p-2">
+          <SummaryTable
+            summaryData={target_date_summary_data}
+            targetDate={target_date}
+            hosId={hos_id}
+          />
 
-  return (
-    <>
-      <MobileTitle title="종합현황" icon={DashboardIcon} />
-
-      <div className="space-y-2 p-2">
-        <SummaryTable
-          summaryData={target_date_summary_data}
-          targetDate={target_date}
-          hosId={hos_id}
-        />
-
-        <SummaryInfos
-          summaryData={target_date_summary_data}
-          targetDate={target_date}
-          prevSummaryCount={prev_summary_count}
-        />
-      </div>
+          <SummaryInfos
+            summaryData={target_date_summary_data}
+            targetDate={target_date}
+            prevSummaryCount={prev_summary_count}
+          />
+        </div>
+      )}
     </>
   )
 }
