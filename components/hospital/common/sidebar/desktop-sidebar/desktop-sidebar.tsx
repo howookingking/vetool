deb4cf05bd6267ef7ae91af3b4f061aa5d52d552
Jungwoo Lee <junsgk@naver.com>
2025-10-25 20:30:29 +0900

    desktop-sidebar

--- components/hospital/common/sidebar/desktop-sidebar/desktop-sidebar.tsx
+++ components/hospital/common/sidebar/desktop-sidebar/desktop-sidebar.tsx
@@ -1,10 +1,10 @@
 import CalculatorSheet from '@/components/hospital/calculator/calculator-sheet'
 import Feedback from '@/components/hospital/feedback/feedback'
-import DesktopSidebarItem from '@/components/hospital/sidebar/desktop/desktop-sidebar-item'
-import SidebarUserDropdown from '@/components/hospital/sidebar/sidebar-user-dropdown'
+import { HOS_SIDEBAR_MENUS } from '@/constants/hospital/hos-sidebar-menus'
 import type { Plan } from '@/constants/plans'
 import type { VetoolUser } from '@/types'
-import { SIDEBAR_MENUS } from '../hospital-sidebar'
+import SidebarUserDropdown from '../sidebar-user-dropdown'
+import DesktopSidebarItem from './desktop-sidebar-item'
 
 type Props = {
   hosId: string
@@ -20,7 +20,7 @@ export default async function DesktopSidebar({
   return (
     <aside className="item-center fixed z-50 hidden h-screen w-10 flex-col justify-between border-r py-1 2xl:flex">
       <ul className="flex flex-col items-center gap-1">
-        {SIDEBAR_MENUS.map(({ icon, isReady, name, path }) => (
+        {HOS_SIDEBAR_MENUS.map(({ icon, isReady, name, path }) => (
           <DesktopSidebarItem
             hosId={hosId}
             name={name}
