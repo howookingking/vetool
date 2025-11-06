c9e1726ef34e6e2edda480c3437dc835ce4e0db0
Jungwoo Lee <junsgk@naver.com>
2025-11-06 15:24:39 +0900

    미사용 라이브러리 삭제

--- components/hospital/message/message.tsx
+++ components/hospital/message/message.tsx
@@ -5,7 +5,7 @@ import { Card } from '@/components/ui/card'
 import type { VetoolUser } from '@/lib/services/auth/authorization'
 import { MessageCircle, X } from 'lucide-react'
 import { useRef, useState } from 'react'
-import Draggable from 'react-draggable'
+// import Draggable from 'react-draggable'
 import MessageWindow from './message-window'
 
 type Props = {
@@ -31,21 +31,21 @@ export default function Message({ loggdedInUser, hosId }: Props) {
       </div>
 
       {isOpen && (
-        <Draggable handle=".handle" nodeRef={nodeRef}>
-          <Card
-            ref={nodeRef}
-            className="fixed bottom-2 right-2 z-50 ml-2 overflow-hidden"
-          >
-            {/* close button */}
-            <X
-              className="absolute right-1 top-1 z-50 h-4 w-4 cursor-pointer transition hover:text-muted-foreground"
-              onClick={() => setIsOpen(false)}
-              aria-label="채팅 닫기"
-            />
+        // <Draggable handle=".handle" nodeRef={nodeRef}>
+        <Card
+          ref={nodeRef}
+          className="fixed bottom-2 right-2 z-50 ml-2 overflow-hidden"
+        >
+          {/* close button */}
+          <X
+            className="absolute right-1 top-1 z-50 h-4 w-4 cursor-pointer transition hover:text-muted-foreground"
+            onClick={() => setIsOpen(false)}
+            aria-label="채팅 닫기"
+          />
 
-            <MessageWindow loggdedInUser={loggdedInUser} hosId={hosId} />
-          </Card>
-        </Draggable>
+          <MessageWindow loggdedInUser={loggdedInUser} hosId={hosId} />
+        </Card>
+        // </Draggable>
       )}
     </>
   )
