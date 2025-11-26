53f458c8e90002fa263f511598129df2bba40146
Jungwoo Lee <87072568+howooking@users.noreply.github.com>
2025-11-26 14:40:42 +0900

    company/ header
    - 로그 크기 감소
    - nav 아이템 hover효과 변경
    - container

--- components/company/header/nav/nav-menu.tsx
+++ components/company/header/nav/nav-menu.tsx
@@ -1,21 +1,24 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
 import { HOMEPAGE_NAVBAR_ITEMS } from '@/constants/company/nav'
 import Link from 'next/link'
 
 export default function NavMenu() {
   return (
-    <ul className="hidden items-center md:flex xl:gap-4">
+    <ul className="hidden items-center gap-8 whitespace-nowrap md:flex">
       {HOMEPAGE_NAVBAR_ITEMS.map((item) => (
         <li key={item.href}>
-          <Button
-            className="text-sm font-semibold leading-6 text-gray-900"
-            asChild
-            variant="link"
+          <Link
+            href={`/${item.href}`}
+            className="group relative flex flex-col items-center"
           >
-            <Link href={`/${item.href}`}>{item.label}</Link>
-          </Button>
+            <span className="invisible font-bold" aria-hidden="true">
+              {item.label}
+            </span>
+            <span className="absolute inset-0 flex items-center justify-center group-hover:font-bold">
+              {item.label}
+            </span>
+          </Link>
         </li>
       ))}
     </ul>
