4639f43398db0f330b2316d3e5148ce8f1aa9e63
Jungwoo Lee <188685705+howookingking@users.noreply.github.com>
2025-12-03 16:03:08 +0900

    og-test

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,25 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'VETOOL - 벳툴',
   description: '동물병원 전문차트 서비스',
+  openGraph: {
+    title: '벳툴',
+    description: '동물병원 전문차트 서비스',
+    url: 'https://vetool.co.kr',
+    images: [
+      {
+        url: 'https://vetool.co.kr/opengraph-image.png',
+        width: 1200,
+        height: 630,
+        type: 'image/png',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '벳툴',
+    description: '동물병원 전문차트 서비스',
+    images: ['https://vetool.co.kr/opengraph-image.png'],
+  },
 }
 
 export default function RootLayout({
