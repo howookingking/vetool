94abb173e81a11d6cf974679389d232a43720810
Jungwoo Lee <188685705+howookingking@users.noreply.github.com>
2025-12-15 16:16:18 +0900

    multi order border

--- components/hospital/icu/main/chart/selected-chart/chart-body/table/order/multil-order/multi-order-dialog.tsx
+++ components/hospital/icu/main/chart/selected-chart/chart-body/table/order/multil-order/multi-order-dialog.tsx
@@ -80,6 +80,8 @@ export default function MultiOrderDialog({
 
         <BorderMultiOrderButton
           setIsMultiOrderDialogOpen={setIsMultiOrderDialogOpen}
+          multiOrderPendingQueue={multiOrderPendingQueue}
+          setMultiOrderPendingQueue={setMultiOrderPendingQueue}
         />
 
         <DeleteSelectedOrdersDialog
