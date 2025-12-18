ba874b306642d4bd197ad609a8fa3a4aa7f07384
Jungwoo Lee <188685705+howookingking@users.noreply.github.com>
2025-12-18 13:21:33 +0900

    autofocus 부작용 해결

--- components/hospital/icu/main/chart/selected-chart/chart-body/table/chart-table.tsx
+++ components/hospital/icu/main/chart/selected-chart/chart-body/table/chart-table.tsx
@@ -8,6 +8,7 @@ import useLocalStorage from '@/hooks/use-local-storage'
 import useOrderSorting from '@/hooks/use-order-sorting'
 import type { SelectedIcuChart } from '@/types/icu/chart'
 import type { OrderWidth } from './chart-table-header/order-width-button'
+import useCellAutofocus from '@/hooks/use-cell-autofocus'
 
 type Props = {
   chartData: SelectedIcuChart
@@ -17,6 +18,8 @@ type Props = {
 
 export default function ChartTable({ chartData, targetDate, hosId }: Props) {
   const { icu_chart_id, orders } = chartData
+
+  useCellAutofocus()
 
   const [orderWidth, setOrderWidth] = useLocalStorage<OrderWidth>(
     'orderWidth',
